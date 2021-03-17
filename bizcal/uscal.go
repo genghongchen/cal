f0ecf8bd26921c204eb871195f0001c9a484df67
genghongchen <55672447+genghongchen@users.noreply.github.com>
2021-03-17 13:10:20 -0700

    Add NYSE calendar

--- bizcal/uscal.go
+++ bizcal/uscal.go
@@ -271,6 +271,90 @@ func (cal USFedCal) IsBusinessDay(t time.Time) bool {
 	return true
 }
 
+//NYSECal, calendar for New York Stock Exchange
+//has all USCal methods
+//It also satisfies BizCal interface
+type NYSECal struct {
+	USCal
+}
+
+//IsBusinessDay checks for business day according to NYSE calendar
+func (cal NYSECal) IsBusinessDay(t time.Time) bool {
+	if cal.IsWeekend(t) {
+		return false
+	}
+
+	y, m, d := t.Date()
+	w := t.Weekday()
+	dd := t.YearDay()
+
+	if cal.IsNewYearsDay(y, m, d, w) ||
+		(y >= 1998 && cal.IsMLKDay(y, m, d, w)) ||
+		cal.IsPresidentsDay(y, m, d, w) ||
+		cal.IsGoodFriday(y, dd) ||
+		cal.IsMemorialDay(y, m, d, w) ||
+		cal.IsIndependenceDay(y, m, d, w) ||
+		cal.IsLaborDay(y, m, d, w) ||
+		cal.IsThanksgiving(y, m, d, w) ||
+		cal.IsChristmas(y, m, d, w) {
+		// holidays
+		return false
+	}
+
+	if (y <= 1968 || (y <= 1980 && y%4 == 0)) &&
+		m == time.November && d <= 7 && w == time.Tuesday {
+		// Presidential election days
+		return false
+	}
+
+	// Special closings
+	if // President Bush's Funeral
+	(y == 2018 && m == time.December && d == 5) ||
+		// Hurricane Sandy
+		(y == 2012 && m == time.October && (d == 29 || d == 30)) ||
+		// President Ford's funeral
+		(y == 2007 && m == time.January && d == 2) ||
+		// President Reagan's funeral
+		(y == 2004 && m == time.June && d == 11) ||
+		// September 11-14, 2001
+		(y == 2001 && m == time.September && (11 <= d && d <= 14)) ||
+		// President Nixon's funeral
+		(y == 1994 && m == time.April && d == 27) ||
+		// Hurricane Gloria
+		(y == 1985 && m == time.September && d == 27) ||
+		// 1977 Blackout
+		(y == 1977 && m == time.July && d == 14) ||
+		// Funeral of former President Lyndon B. Johnson.
+		(y == 1973 && m == time.January && d == 25) ||
+		// Funeral of former President Harry S. Truman
+		(y == 1972 && m == time.December && d == 28) ||
+		// National Day of Participation for the lunar exploration.
+		(y == 1969 && m == time.July && d == 21) ||
+		// Funeral of former President Eisenhower.
+		(y == 1969 && m == time.March && d == 31) ||
+		// Closed all day - heavy snow.
+		(y == 1969 && m == time.February && d == 10) ||
+		// Day after Independence Day.
+		(y == 1968 && m == time.July && d == 5) ||
+		// June 12-Dec. 31, 1968
+		// Four day week (closed on Wednesdays) - Paperwork Crisis
+		(y == 1968 && dd >= 163 && w == time.Wednesday) ||
+		// Day of mourning for Martin Luther King Jr.
+		(y == 1968 && m == time.April && d == 9) ||
+		// Funeral of President Kennedy
+		(y == 1963 && m == time.November && d == 25) ||
+		// Day before Decoration Day
+		(y == 1961 && m == time.May && d == 29) ||
+		// Day after Christmas
+		(y == 1958 && m == time.December && d == 26) ||
+		// Christmas Eve
+		((y == 1954 || y == 1956 || y == 1965) && m == time.December && d == 24) {
+		return false
+	}
+
+	return true
+}
+
 // End QuantLib code adaptation
 
 //AdjForBusinessDay take one date and either returns itself
@@ -294,6 +378,33 @@ func NextBusinessDay(cal BizCal, t time.Time) time.Time {
 
 	for {
 		rt = rt.AddDate(0, 0, 1)
+		if cal.IsBusinessDay(rt) {
+			return rt
+		}
+	}
+}
+
+//AdjLastBusinessDay take one date and either returns itself
+//if it is already a business day
+//or returns the last business day
+func AdjLastBusinessDay(cal BizCal, t time.Time) time.Time {
+	rt := t
+
+	for {
+		if cal.IsBusinessDay(rt) {
+			return rt
+		}
+		rt = rt.AddDate(0, 0, -1)
+	}
+}
+
+//PrevBusinessDay takes one day and returns
+//the previous business day before that day
+func PrevBusinessDay(cal BizCal, t time.Time) time.Time {
+	rt := t
+
+	for {
+		rt = rt.AddDate(0, 0, -1)
 		if cal.IsBusinessDay(rt) {
 			return rt
 		}
